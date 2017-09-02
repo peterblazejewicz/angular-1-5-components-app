6e58c0b7122f8dd068fa8d11cc026cdb65838012
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-02 22:45:28 +0200

    Small fixes/changes

--- src/app/common/app.controller.js
+++ src/app/common/app.controller.js
@@ -2,15 +2,15 @@ function AppController(AuthService, $state) {
   var ctrl = this;
   ctrl.user = AuthService.user;
 
- /**
+  /**
   * @ngdoc method
   * @name AppController#logout
   *
   * @description Logout :)
   */
-  ctrl.logout = function () {
-    AuthService.logout().then(function () {
-      $state.go('auth.login');
+  ctrl.logout = function() {
+    AuthService.logout().then(function() {
+      $state.go("auth.login");
     });
   };
 }
@@ -30,6 +30,4 @@ function AppController(AuthService, $state) {
  * Aenean ornare odio elit, eget facilisis ipsum molestie ac. Nam bibendum a nibh ut ullamcorper.
  * Donec non felis gravida, rutrum ante mattis, sagittis urna. Sed quam quam, facilisis vel cursus at.
  */
-angular
-  .module('common')
-  .controller('AppController', AppController);
+angular.module("common").controller("AppController", AppController);
